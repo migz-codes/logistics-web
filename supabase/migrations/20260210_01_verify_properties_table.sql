d6112463b96adeb214ca67cd010859cfb7a6f725
migz <migz.codes@gmail.com>
2026-02-11 22:21:16 -0300

    fix: update migrations

--- supabase/migrations/20260210_01_verify_properties_table.sql
+++
@@ -1,9 +0,0 @@
--- Verify properties table structure
-SELECT 
-    column_name, 
-    data_type, 
-    is_nullable, 
-    column_default
-FROM information_schema.columns 
-WHERE table_name = 'properties' 
-ORDER BY ordinal_position;
